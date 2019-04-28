6aa3913b4a5a55cf50b52010cff1615da2278060
Sergei Orlov <sorlov+github@gmail.com>
2019-04-28 19:47:56 +0000

    Better logging

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+LOGS=~/puller_start_logs.txt
 export FLASK_APP=~/code/puller/puller.py
-~/code/puller/venv/bin/flask run --host=0.0.0.0 --port=3003 2>&1 >> ~/puller_start_logs.txt
+
+echo "\n\$(date) starting puller" >> "$LOGS"
+~/code/puller/venv/bin/flask run --host=0.0.0.0 --port=3003 2>&1 >> "$LOGS"
