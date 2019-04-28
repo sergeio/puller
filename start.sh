09a68c8b7608da716ba74050b8d871aedcd186a0
Sergei Orlov <sorlov+github@gmail.com>
2019-04-28 13:54:58 -0700

    Fixups

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LOGS=~/abacus_start_logs.txt
+LOGS=~/puller_start_logs.txt
 export FLASK_APP=~/code/puller/puller.py
 
 echo -e "\n$(date) starting puller" >> "$LOGS"
-~/code/puller/venv/bin/flask run --host=0.0.0.0 --port=8080 >> "$LOGS" 2>&1
+~/code/puller/venv/bin/flask run --host=0.0.0.0 --port=3003 >> "$LOGS" 2>&1
